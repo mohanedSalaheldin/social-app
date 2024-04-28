c142fcf1ad9ee2894b07bdcb25ff04aa58b971f9
mohanedSalaheldin <mohnsd1162002@gmail.com>
2024-04-28 16:13:57 +0300

    edit post widget

--- lib/src/features/home/presentation/pages/home_screen.dart
+++ lib/src/features/home/presentation/pages/home_screen.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:hexcolor/hexcolor.dart';
 
@@ -22,175 +20,10 @@ class HomeScreen extends StatelessWidget {
               icon: const Icon(Icons.email_outlined),
             ),
           ]),
-      body: Column(
-        children: [
-          Container(
-            color: const Color.fromARGB(255, 237, 236, 236),
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const CircleAvatar(
-                      radius: 30.0,
-                      backgroundImage:
-                          NetworkImage('https://i.pravatar.cc/300'),
-                    ),
-                    const Gap(10.0),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'username',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                          ),
-                        ),
-                        Text(
-                          'time',
-                          style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.more_vert_outlined),
-                    ),
-                  ],
-                ),
-                const Gap(10.0),
-                const Text(
-                  'lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet',
-                ),
-                const Gap(10.0),
-                Image.network(
-                  'https://i.pravatar.cc/700',
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                ),
-                const Gap(10.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.favorite_outline),
-                        ),
-                        const Gap(10.0),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.chat_bubble_outline),
-                        ),
-                        const Gap(10.0),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.send_outlined),
-                        ),
-                      ],
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.bookmark_outline),
-                    ),
-                  ],
-                ),
-                const Gap(10.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsetsDirectional.only(start: 0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50.0),
-                              border: Border.all(
-                                width: 3.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                            child: const CircleAvatar(
-                              radius: 15.0,
-                              backgroundImage:
-                                  NetworkImage('https://i.pravatar.cc/300'),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.only(start: 15.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50.0),
-                              border: Border.all(
-                                width: 3.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                            child: const CircleAvatar(
-                              radius: 15.0,
-                              backgroundImage:
-                                  NetworkImage('https://i.pravatar.cc/500'),
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsetsDirectional.only(start: 30.0),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(50.0),
-                              border: Border.all(
-                                width: 3.0,
-                                color: Colors.white,
-                              ),
-                            ),
-                            child: const CircleAvatar(
-                              radius: 15.0,
-                              backgroundImage:
-                                  NetworkImage('https://i.pravatar.cc/400'),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const Gap(10.0),
-                    RichText(
-                      text: const TextSpan(
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          color: Colors.black,
-                        ),
-                        children: [
-                          TextSpan(
-                            text: 'liked by ',
-                          ),
-                          TextSpan(
-                            text: 'Joe Hoe',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          TextSpan(
-                            text: ' and 1.5k',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-        ],
+      body: ListView.separated(
+        itemBuilder: (context, index) => const RootWidget(),
+        separatorBuilder: (context, index) => const Gap(10.0),
+        itemCount: 5,
       ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (value) => {},
@@ -198,9 +31,10 @@ class HomeScreen extends StatelessWidget {
         selectedItemColor: HexColor('#7737ff'),
         type: BottomNavigationBarType.fixed,
         elevation: 0.0,
+        // iconSize: 30.0,
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
             label: '',
           ),
           BottomNavigationBarItem(
@@ -208,15 +42,15 @@ class HomeScreen extends StatelessWidget {
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
+            icon: Icon(Icons.add_circle_outline),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.mail),
+            icon: Icon(Icons.favorite_border_outlined),
             label: '',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person_outline),
             label: '',
           ),
         ],
---
+++ lib/src/features/home/presentation/widgets/post_widget.dart
@@ -0,0 +1,179 @@
+
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class PostWidget extends StatelessWidget {
+  const PostWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      // decoration: BoxDecoration(
+      //   borderRadius: BorderRadius.circular(20.0),
+      //   // color: const Color.fromARGB(255, 242, 243, 245),
+      // ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              const CircleAvatar(
+                radius: 30.0,
+                backgroundImage: NetworkImage('https://i.pravatar.cc/300'),
+              ),
+              const Gap(10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'username',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  Text(
+                    'time',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.grey[700],
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ],
+          ),
+          const Gap(10.0),
+          const Text(
+            'lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit amet',
+          ),
+          const Gap(10.0),
+          Image.network(
+            'https://i.pravatar.cc/700',
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+          const Gap(10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.favorite_outline),
+                  ),
+                  const Gap(2.0),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.chat_bubble_outline),
+                  ),
+                  const Gap(2.0),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.send_outlined),
+                  ),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.bookmark_outline),
+              ),
+            ],
+          ),
+          const Gap(10.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(start: 0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        border: Border.all(
+                          width: 3.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: const CircleAvatar(
+                        radius: 15.0,
+                        backgroundImage:
+                            NetworkImage('https://i.pravatar.cc/300'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(start: 15.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        border: Border.all(
+                          width: 3.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: const CircleAvatar(
+                        radius: 15.0,
+                        backgroundImage:
+                            NetworkImage('https://i.pravatar.cc/500'),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(start: 30.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50.0),
+                        border: Border.all(
+                          width: 3.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: const CircleAvatar(
+                        radius: 15.0,
+                        backgroundImage:
+                            NetworkImage('https://i.pravatar.cc/400'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(10.0),
+              RichText(
+                text: const TextSpan(
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'liked by ',
+                    ),
+                    TextSpan(
+                      text: 'Joe Hoe',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' and 1.5k',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
